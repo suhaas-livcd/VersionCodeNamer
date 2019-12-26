59dd72455e42bac7406ded302be508d095db642e
suhaas-livcd <suhaas-livcd@users.noreply.github.com>
2019-12-26 17:32:41 +0530

    Initial commit

---
+++ versioncodenamerlib/src/main/java/com/hosteloha/versioncodenamerlib/ListOf.java
@@ -0,0 +1,8 @@
+package com.hosteloha.versioncodenamerlib;
+
+public class ListOf {
+    public static final int _chemicalElements = 1;
+    public static final int _englishNumbers = _chemicalElements + 1;
+    public static final int _travelPlacesNature = _englishNumbers + 1;
+
+}
---
+++ versioncodenamerlib/src/main/java/com/hosteloha/versioncodenamerlib/VersionCodeNamer.java
@@ -0,0 +1,18 @@
+package com.hosteloha.versioncodenamerlib;
+
+public class VersionCodeNamer {
+
+    public final static int Default = -1;
+
+    public static void main(String[] args) {
+
+        getName(0,0);
+    }
+    /**
+     * @param versionCode
+     * @param versionType
+     */
+    public static void getName(int versionCode, int versionType) {
+
+    }
+}
